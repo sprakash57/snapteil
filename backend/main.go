4effb4bb1260a5d83de03f02df7b0ef386a258c3
sunny <sunny.prakashgm@gmail.com>
2026-03-27 19:22:22 +0530

    feat: add air and setup logger middleware for better development workflow

--- backend/main.go
+++ backend/main.go
@@ -4,15 +4,19 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/sprakash57/snaptiel/backend/handlers"
 )
 
 func main() {
 	app := fiber.New()
 
+	// middelewares
+	app.Use(logger.New())
+
 	apiV1 := app.Group("/api/v1")
 
-	apiV1.Get("/health", handlers.CheckHealthHandler())
+	apiV1.Get("/health", handlers.CheckHealthHandler()) // can be used by load balancers to check if the server is healthy
 
 	log.Fatal(app.Listen(":4000"))
 }
